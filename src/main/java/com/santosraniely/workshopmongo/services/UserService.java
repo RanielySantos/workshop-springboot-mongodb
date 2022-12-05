6a433f4020414ec645a541578a792ab97baeb32f
RanielySantos <53355463+RanielySantos@users.noreply.github.com>
2022-12-05 12:10:43 -0300

    Obtendo um usuario por id

--- src/main/java/com/santosraniely/workshopmongo/services/UserService.java
+++ src/main/java/com/santosraniely/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.santosraniely.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.santosraniely.workshopmongo.domain.User;
 import com.santosraniely.workshopmongo.repository.UserRepository;
+import com.santosraniely.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
 	}
 }
