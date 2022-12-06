1e539a5fe93593545ca8624f75c651a997569361
RanielySantos <53355463+RanielySantos@users.noreply.github.com>
2022-12-06 12:15:43 -0300

    Obtendo post por id

---
+++ src/main/java/com/santosraniely/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.santosraniely.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.santosraniely.workshopmongo.domain.Post;
+import com.santosraniely.workshopmongo.repository.PostRepository;
+import com.santosraniely.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+	}
+	
+}
